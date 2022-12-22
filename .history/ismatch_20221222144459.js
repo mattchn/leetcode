044c9e891dd08a3118ff25347bf08afae631ab27
Matthew Chan <mattchan1999@gmail.com>
2022-12-22 14:45:10 -0500

    ismatch

---
+++ .history/ismatch_20221222144459.js
@@ -0,0 +1,12 @@
+var isMatch = function(s, p) {
+   if (p.length === 0) return s.length === 0;
+  let firstMatch = (s.length !== 0 && (p[0] === s[0] || p[0] === '.'));
+  if (p.length >= 2 && p[1] === '*') {
+      return (isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p)));
+  } else {
+      return firstMatch && isMatch(s.substring(1), p.substring(1));
+  }
+}
+
+
+console.log(isMatch('aa', 'a'))
