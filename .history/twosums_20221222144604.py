8969d5ab5f9eec8adee331e2e63017815df3b66e
Matthew Chan <mattchan1999@gmail.com>
2022-12-22 14:47:19 -0500

    twosum

---
+++ .history/twosums_20221222144604.py
@@ -0,0 +1,17 @@
+def twoSum(self, nums, target):
+   # List to store results
+   result = []
+   # Dictionary to store the difference and its index
+   index_map = {}
+   # Loop for each element
+   for i, n in enumerate(nums):
+      # Difference which needs to be checked
+      difference = target - n
+      if difference in index_map:
+            result.append(i)
+            result.append(index_map[difference])
+            break
+      else:
+            index_map[n] = i
+   return result
+
---
+++ .history/twosums_20221222144619.py
@@ -0,0 +1,19 @@
+def twoSum(self, nums, target):
+   # List to store results
+   result = []
+   # Dictionary to store the difference and its index
+   index_map = {}
+   # Loop for each element
+   for i, n in enumerate(nums):
+      # Difference which needs to be checked
+      difference = target - n
+      if difference in index_map:
+            result.append(i)
+            result.append(index_map[difference])
+            break
+      else:
+            index_map[n] = i
+   return result
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 2, 4], 6))
