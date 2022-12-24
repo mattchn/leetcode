392a9691dfdcbe6d12b1a458bd45785e94b83337
Matthew Chan <mattchan1999@gmail.com>
2022-12-24 11:42:35 -0500

    longest common prefix

---
+++ longestCommonPrefix.py
@@ -0,0 +1,16 @@
+def longestCommonPrefix(strs):
+   """
+   :type strs: List[str]
+   :rtype: str
+   """
+   if not strs:
+      return ""
+   if len(strs) == 1:
+      return strs[0]
+   prefix = strs[0]
+   for i in range(1, len(strs)):
+      while strs[i].find(prefix) != 0:
+         prefix = prefix[:-1]
+         if not prefix:
+               return ""
+   return prefix
