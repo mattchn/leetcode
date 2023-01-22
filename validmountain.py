6bf1eb6ec85312451bd6b010d0a71745b7242004
Matthew Chan <mattchan1999@gmail.com>
2023-01-22 15:09:27 -0500

    valid mountain

---
+++ validmountain.py
@@ -0,0 +1,11 @@
+def validMountainArray(arr):
+   if len(arr) < 3:
+      return False
+   i = 0
+   while (i < len(arr) - 1 and arr[i] < arr[i + 1]):
+      i += 1
+   if (i == 0 or i == len(arr) - 1):
+      return False
+   while (i < len(arr) - 1 and arr[i] > arr[i + 1]):
+      i += 1
+   return i == len(arr) - 1
