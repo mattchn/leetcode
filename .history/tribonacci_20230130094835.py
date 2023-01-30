5c2f6ea533a760a51f0e4ab3e7819592f425cbe0
Matthew Chan <mattchan1999@gmail.com>
2023-01-30 09:49:38 -0500

    tribonacci

---
+++ .history/tribonacci_20230130094835.py
@@ -0,0 +1,10 @@
+def tribonacci(n):
+   if n == 0:
+      return 0
+   elif n == 1 or n == 2:
+      return 1
+   else:
+      t = [0, 1, 1]
+      for i in range (3, n + 1):
+         t.append(t[i - 1] + t[i - 2], t[i - 3])
+      return t
