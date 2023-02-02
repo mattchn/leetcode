bf12ef55b961e605a2b2678ec5496807a177ae06
Matthew Chan <mattchan1999@gmail.com>
2023-02-02 16:16:44 -0500

    symmetrictree

---
+++ symmetrcitree.py
@@ -0,0 +1,14 @@
+class TreeNode:
+   def __init__(self, val=0, left=None, right=None):
+      self.val = val
+      self.left = left
+      self.right = right
+
+def isSymmetric(root):
+   def isMirror(t1, t2):
+      if not t1 and not t2:
+         return True
+      if not t1 or not t2:
+         return False
+      return t1.val == t2.val and isMirror(t1.right, t2.left) and isMirror(t1.left, t2.right)
+   return isMirror(root, root)
